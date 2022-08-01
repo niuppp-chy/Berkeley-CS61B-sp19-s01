8304a194e4d1c0216a494646c53801cd872fef56
niuppp-chy <949983648@qq.com>
2022-08-01 07:34:37 -0700

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,10 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+		int x = 0;
+        while (x < 10) {
+            System.out.print(sum + " ");
+            sum += x;
+        }
+    }
+}
